9e8648c6431e4712734d6655f3ec8a8a56b1e77b
Alexander Douglas <xjdouglas05@gmail.com>
2025-09-25 09:43:30 -0400

    did part of hw 3, finished hw 2

--- hw2_Douglas_Alexander.py
+++ hw2_Douglas_Alexander.py
@@ -24,9 +24,43 @@ def problem3():
     last_column = arr[:, 4]
     center_subarray = arr[1:3,1:3]
     greater_than_15 = arr[arr > 15]
-    
-#def problem4():
-    
-#def problem5():
+    arr_copy = arr.copy()
+    for prt in arr_copy:
+        if prt%2 == 0:
+            prt = prt-1
+    return third_row, last_column, center_subarray, greater_than_15, arr_copy
+
+def problem4():
+    scores = np.array([[85, 90, 78, 92],
+        [79, 85, 88, 91],
+        [92, 88, 95, 89],
+        [75, 72, 80, 78],
+        [88, 91, 87, 94]])
+    student_averages = np.mean(scores, axis=1)
+    test_averages = np.mean(scores, axis=0)
+    student_max_scores = np.max(scores, axis=1)
+    test_std = np.std(scores, axis=0)
+    high_performers = student_averages[student_averages>85]
+    return student_averages, test_averages, student_max_scores, test_std, high_performers
+
+def problem6():
+    import time
+    size = 100000
+    python_list = list(range(size))
+    numpy_array = np.arange(size)
+    start_time = time.time()
+    list_result = [x**2 for x in python_list]
+    list_time = time.time() - start_time
+    start_time = time.time()
+    array_result = numpy_array**2
+    numpy_time = time.time() - start_time
+    speedup = list_time/numpy_array
+    return  {
+        'list_time': list_time,
+        'numpy_time': numpy_time,
+        'speedup': speedup,
+        'conclusion': f"NumPy is {speedup:.1f}x faster than Python lists for this operation"
+        }
+
 #test = problem1()
 #print(test)
